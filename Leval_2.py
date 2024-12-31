b6a4d134b9a31d47cd1b9c02188577494f343dc7
hat-cod <hat.hacker.me@gmail.com>
2024-12-31 12:02:17 -0800

    add

--- Leval_2.py
+++ Leval_2.py
@@ -2,5 +2,7 @@ import time  # import time in install
 import random   # import random install
 class Leval2:    # class created
     def __init__(self):  # function 
-        
-        
+        self.start_leval_2 = {}  # start conditons adding 
+        self.sound_tone = {}  # sound  comdotons adding
+        self. 
+
